6b6b513b8ea89d24781e92187646912f9c508fb2
cdev2025 <creativedev2025@gmail.com>
2025-11-28 10:33:49 +0900

    [2025-11-28] pages > LoginPage 작업중...

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,7 +1,71 @@
-import React from "react";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import React, { useState } from "react";
+import ApiService from "../api/ApiService";
 
 function LoginPage() {
-  return <div>LoginPage</div>;
+  // 입력 폼 상태
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  // input 변경 핸들러
+  const onChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // 로그인 요청 함수
+  const login = async () => {
+    try {
+      const response = await ApiService.post("/open-api/user/login", {
+        result: {},
+        body: form,
+      });
+
+      console.log("로그인 응답: ", response);
+
+      if (!response.body) {
+        alert("서버 응답 형식 오류: body 없음");
+        return;
+      }
+
+      // accessToken 저장
+
+      // 로그인 성공 메시지 띄우고, 홈으로 이동
+    } catch (error) {
+      console.error("로그인 실패: ", error);
+      alert("로그인 실패");
+    }
+  };
+
+  return (
+    <Box>
+      <Typography variant="h5">로그인</Typography>
+
+      <TextField
+        label="이메일"
+        name="email"
+        fullWidth
+        margin="normal"
+        onChange={onChange}
+      />
+
+      <TextField
+        label="비밀번호"
+        name="password"
+        fullWidth
+        margin="normal"
+        onChange={onChange}
+      />
+
+      <Button variant="contained" fullWidth sx={{ mt: 2 }} onClick={login}>
+        로그인
+      </Button>
+    </Box>
+  );
 }
 
 export default LoginPage;
